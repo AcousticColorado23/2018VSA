73306745ef2cbaca05db1ecfe04d228d04e3031e
AcousticColorado23 <40045803+AcousticColorado23@users.noreply.github.com>
2018-06-11 15:24:41 -0500

    Add files via upload

--- proj01_ifelse/proj01.py
+++ proj01_ifelse/proj01.py
@@ -24,12 +24,23 @@ print name
 # Then, it prints a sentence that says the number of days and months until their birthday
 
 print"hi"
-month = int( raw_input("What is your birth month (number)?") )
-day = int (raw_input("What is your birthday (number)?") )
-cm = int (raw_input("What is the current month (number)?") )
-cd = int (raw_input("What is you current day (number)?") )
- if month >= cm:
-     print "Your birthday is in " + str(month - cm) + " months!"
- if day >= cd:
-     print "Your birthday is in " + str(day - cd)  + " days!"
+month = int(raw_input("What is your birth month (number)?"))
+day = int(raw_input("What is your birthday (number)?"))
+cm = int(raw_input("What is the current month (number)?"))
+cd = int(raw_input("What is you current day (number)?"))
+if month >= cm:
+    print "Your birthday is in " + str(month - cm) + " months!"
+elif cm >= month:
+    print "Your birthday is in " + str(12-(cm - month)) + " months!"
+if day >= cd:
+    print "Your birthday is in " + str(day - cd)  + " days!"
+elif cd >= day:
+    print "Your birthday is in " + str(30 -(cd - day)) + " days!"
+
+    
+
+
+
+
+
 # If you complete extensions, describe your extensions here!
