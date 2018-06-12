1a449807d849b35ac53fba1ceb718e4f41f18947
AcousticColorado23 <40045803+AcousticColorado23@users.noreply.github.com>
2018-06-12 15:35:52 -0500

    Add files via upload

--- proj02_loops/proj02_02.py
+++ proj02_loops/proj02_02.py
@@ -10,4 +10,38 @@ previous two numbers in the sequence. The sequence looks like this:
 1, 1, 2, 3, 5, 8, 13...
 """
 
+pn = 0
+cn = 1
+nn = 1
+
+
+x = int(raw_input("Enter a number:"))
+for f in range(1,x):
+    print cn
+    pn = cn
+    cn = nn
+    nn = cn + pn
+
+pn = 1
+cn = 2
+nn = 4
+
+# Powers of 2 Extensions
+
+x = int(raw_input("Enter a number:"))
+for f in range(1,x):
+    print cn
+    pn = cn
+    cn = nn
+    nn = cn + pn
+
+
+
+
+
+
+x = int(raw_input("Enter a number:"))
+for f in range(1,x):
+    if x%f == 0:
+        print f
 
