fdb5fdaef473785494827e0b06a31a7e5c43e434
AcousticColorado23 <40045803+AcousticColorado23@users.noreply.github.com>
2018-06-13 15:20:08 -0500

    Add files via upload

--- proj04_lists/proj04.py
+++ proj04_lists/proj04.py
@@ -31,33 +31,56 @@ c = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 # that are common between the lists (without duplicates).
 # Make sure your program works on two lists of different sizes.
 
-for item in b:
-    if item in b:
-        print item in c
 
+for item in b:
+    for item_c in c:
+        if item == item_c:
+            print item
 
 
 
 
 #Part III
 # Take a list, say for example this one:
-
-d = ["b", "a", "f", "y", "a", "t", "_", "p", "a", "R"]
 # and write a program that replaces all “a” with “*”.
 
+d = ["b", "a", "f", "y", "a", "t", "_", "p", "a", "R"]
+on = ("a")
+nn = ("*")
+counter = 0
 
-
-
-
-
-
+for item in d:
+    if d[counter] == on:
+        d[counter] = nn
+    counter = counter + 1
+print d
 
 
 #Part IV
 #Ask the user for a string, and print out whether this string is a palindrome or not.
 
+l2 = []
+l = []
+song = raw_input("Please enter a word. ")
 
+for letter in song:
+    l.append(letter)
+    l2.append(letter)
+print l
+l2.reverse()
+print l2
+l3 = []
 
+l = []
+song = raw_input("Please enter a word. ")
+for letter in song:
+    l.append(letter)
+    l2.append(letter)
+print l
+for num in range(0, len(song)):
+    let = len(song) - num -1
+    l3.append(l[let])          
+print l3
 
 
 
